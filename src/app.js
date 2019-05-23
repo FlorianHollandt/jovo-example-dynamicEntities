9d1c582e886ce0640144a3f9e73128d5cb0119b4
FlorianHollandt <florian.hollandt@gmail.com>
2019-05-23 17:07:34 +0200

    :sparkles: Removed dependency

--- src/app.js
+++ src/app.js
@@ -6,16 +6,12 @@
 
 const { App } = require('jovo-framework');
 const { Alexa } = require('jovo-platform-alexa');
-// const { GoogleAssistant } = require('jovo-platform-googleassistant');
-// const { JovoDebugger } = require('jovo-plugin-debugger');
 const { FileDb } = require('jovo-db-filedb');
 
 const app = new App();
 
 app.use(
     new Alexa(),
-    // new GoogleAssistant(),
-    // new JovoDebugger(),
     new FileDb()
 );
 
